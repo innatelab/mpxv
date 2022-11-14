e8e5ac35d4e8ed3d91271559503e75daf3192de1
Yiqi Huang <yiqi.huang@tum.de>
2022-11-14 18:13:55 +0100

    update image path

--- msglm_fit_chunk_fp.lrz.sh
+++ msglm_fit_chunk_fp.lrz.sh
@@ -30,7 +30,7 @@
 module load slurm_setup
 module load charliecloud
 
-IMAGES_PATH=$(realpath $HOME)
+IMAGES_PATH=$(realpath $HOME/docker4muc)
 CHUDIS_PATH=$HOME/projects/cool_chunk_dispatcher
 
 PROJECT_ID=mpxv
